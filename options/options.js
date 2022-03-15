eb2bd13f15f0c897c0cac643746918b9a6a00112
Crowdbotics <team@crowdbotics.com>
2022-03-15 22:00:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "flip_wall_street_34220",
+  url: "https://flip_wall_street_34220.botics.co",
+  api: "https://flip_wall_street_34220.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
